be9e284bddc887d8f89027f9f17a5f11618369e4
Rafael Medeiros <rafaelmedrib@gmail.com>
2024-01-08 20:38:42 -0300

    Create a sample Math Class

---
+++ index.js
@@ -0,0 +1,55 @@
+/**
+ * @constructor
+ */
+
+function MathClass (){
+  publicApi = {
+    /**
+    * @param {number} a
+    * @param {number} b
+    * @return {number}
+    * @public
+    * @method
+    */
+    addTwoNumbers: function(a, b){
+      return a + b;
+    },
+    /**
+     * 
+     * @param {number} a
+     * @param {number} b 
+     * @returns {number}
+     * @public
+     * @method
+     */
+    subtractTwoNumbers: function(a, b){
+      return a - b;
+    },
+    /**
+     * 
+     * @param {number} a 
+     * @param {number} b 
+     * @returns {number}
+     * @public
+     * @method
+     */
+    multiplyTwoNumbers: function(a, b){
+      return a * b;
+    },
+    /**
+     * 
+     * @param {number} a 
+     * @param {number} b 
+     * @returns {number}
+     * @public
+     * @method
+     */
+    divideTwoNumbers: function(a, b){
+      return a / b;
+    }
+  }
+
+  return publicApi;
+}
+
+module.exports = MathClass;
